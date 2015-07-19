17e6ead0e800a99f5a84f00b5e12d04bd1aefefe
KaiKikuchi <kai.kikuchi27@gmail.com>
2015-07-20 01:24:13 +0200

    Removed Hashmap from Messages class

--- src/main/java/net/kaikk/mc/gppcities/Messages.java
+++ src/main/java/net/kaikk/mc/gppcities/Messages.java
@@ -20,7 +20,6 @@ package net.kaikk.mc.gppcities;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.logging.Level;
 
 import org.bukkit.ChatColor;
@@ -43,134 +42,132 @@ public enum Messages {
 	TakeableOn, TakeableOff, PlayerBannedConfirm, PlayerUnbannedConfirm, PlotInfo, MayorCannotLeave, YouOnUnassignedPlot,
 	YouOnTakeablePlot, YouOnJoinableCity, CitySpawnTeleportDelay, CitySpawnTeleportCancelled, CitySpawnTeleported;
 
-	static private HashMap<Messages, String> messages = new HashMap<Messages, String>();
 	final static String messagesFilePath = "plugins" + File.separator + "GriefPreventionPlus-Cities" + File.separator + "messages.yml";
-	
+	private String text;
+
 	static void defaults() {
-		messages.put(Messages.CantRunCommandYouHaveCity, "&cYou can't run this command: claim ID &b{0} &cis a city named &b{1}&c!");
-		messages.put(Messages.CitiesList, "&aCities list:");
-		messages.put(Messages.CitiesListFormat, "{0} [&e{1}&f]");
-		messages.put(Messages.CitiesNotAvailableOnAdminClaims, "&cCities are not available on admin claims.");
-		messages.put(Messages.CitizenExpelled, "&aCitizen expelled.");
-		messages.put(Messages.CityAutojoinNotPermitted, "&c{0} does not permit auto-join, ask for an invite.");
-		messages.put(Messages.CityAutojoinOff, "&aNow players need an invitation to join this city.");
-		messages.put(Messages.CityAutojoinOn, "&aNow players can join this city without an invitation.");
-		messages.put(Messages.CityHasBeenDisbanded, "&c{0} has been disbanded!");
-		messages.put(Messages.CityInvitationAcceptOrReject, "&aType /c accept to accept; otherwise /c reject.");
-		messages.put(Messages.CityInvitationAccepted, "&aYou accepted the invitation.");
-		messages.put(Messages.CityInvitationExpiresOn, "&aThis player already recieved an invitation, it expires in: {0} seconds.");
-		messages.put(Messages.CityInvitationHasBeenAccepted, "&aYour invitation to &b{0} &ahas been &baccepted&a.");
-		messages.put(Messages.CityInvitationHasBeenRejected, "&aYour invitation to &b{0} &ahas been &crejected&a.");
-		messages.put(Messages.CityInvitationNoPending, "&cYou do not have a pending invitation.");
-		messages.put(Messages.CityInvitationReceived, "&aYou received a city invitation from &b{0} &ato join &b{1}&a.");
-		messages.put(Messages.CityInvitationRejected, "&aYou rejected the invitation.");
-		messages.put(Messages.CityInvitationSent, "&aCity invitation sent to &b{0}.");
-		messages.put(Messages.CityNotExists, "&cThe city &b{0} &cdoes not exist.");
-		messages.put(Messages.CitySpawnInvalid, "&cCity spawn must be located inside your city.");
-		messages.put(Messages.CitySpawnMissing2, "&c{0}'s spawn is missing.");
-		messages.put(Messages.CitySpawnSet, "&aCity spawn set.");
-		messages.put(Messages.ClaimAlreadyACity, "&cThis claim is already a city.");
-		messages.put(Messages.InviteNotAllowed, "&cYou're not allowed to invite people.");
-		messages.put(Messages.MotdHasBeenSet, "&aA new motd has been set:");
-		messages.put(Messages.MotdOut, "&aOuters motd: &b{0}");
-		messages.put(Messages.MotdRes, "&aResidents motd: &b{0}");
-		messages.put(Messages.MotdSet, "&aMotd set.");
-		messages.put(Messages.NewCity, "&a{0} has just founded a new city called &b{1}&a.");
-		messages.put(Messages.NoCities, "&aThere's no city! Be the first! Make a claim, and type /city new CityName.");
-		messages.put(Messages.NoPermission, "&cYou don't have permission to use this command.");
-		messages.put(Messages.NotAuthorized, "&cYou're not authorized.");
-		messages.put(Messages.PlayerAlreadyOnAnotherCity, "&cThis player is already a member of another city.");
-		messages.put(Messages.PlayerIsNotACitizen, "&cThis player is not a citizen.");
-		messages.put(Messages.PlayerJoinedYourCity, "&b{0} &ajoined your city!");
-		messages.put(Messages.PlayerLeftCity, "&b{0} &aleft the city.");
-		messages.put(Messages.PlayerOfflineOrWrongName, "&cThis player is offline or the player name is wrong.");
-		messages.put(Messages.PlotAssigned, "&aPlot assigned.");
-		messages.put(Messages.PlotTakeNotAllowed, "&cYou can take this plot.");
-		messages.put(Messages.PlotTakeYouHaveAPlotAlready, "&cYou can't take another plot if you have an assigned plot already.");
-		messages.put(Messages.PlotCreated, "&aNew plot created.");
-		messages.put(Messages.PlotDeleted, "&aPlot deleted.");
-		messages.put(Messages.PlotMotdEdited, "&aPlot motd edited.");
-		messages.put(Messages.PlotUnassigned, "&aPlot unassigned.");
-		messages.put(Messages.StandOnPlotOrSubClaim, "&cYou have to stand on a subdivision. Use /subdivideclaims.");
-		messages.put(Messages.ThisCityNotExists, "&cThis city doesn't exist.");
-		messages.put(Messages.WelcomeBackTo, "&2Welcome back to &a{0}.");
-		messages.put(Messages.WelcomeTo, "&2Welcome to &a{0}.");
-		messages.put(Messages.WrongCitizenName, "&cWrong citizen name or this citizen is not part of your city.");
-		messages.put(Messages.WrongCityName, "&cWrong city name.");
-		messages.put(Messages.WrongCityNameCS, "&cWrong city name (it's case-sensitive).");
-		messages.put(Messages.WrongCommand, "&cWrong command.");
-		messages.put(Messages.YouAlreadyOnAnotherCity, "&cYou're a member of another city.");
-		messages.put(Messages.YouCantRunCommandOnACity, "&cYou can't run this command on a city.");
-		messages.put(Messages.YouLeftCity, "&aYou left &b{0}.");
-		messages.put(Messages.YouNotACitizenHere, "&cYou're not a citizen here.");
-		messages.put(Messages.YouNotCitizen, "&cYou're not a citizen.");
-		messages.put(Messages.YouNotInACity, "&cYou're not in a city.");
-		messages.put(Messages.YouNotOnAClaim, "&cYou're not on a claim.");
-		messages.put(Messages.YouOnClaim, "&aYou're on &b{0}&a's claim.");
-		messages.put(Messages.YouOnPlot, "&aYou entered &b{0}&a's plot.");
-		messages.put(Messages.YouOnWilderness, "&aYou entered the wilderness.");
-		messages.put(Messages.YouveBeenExpelled, "&eYou've been expelled from &b{0}.");
+		Messages.CantRunCommandYouHaveCity.text = "&cYou can't run this command: claim ID &b{0} &cis a city named &b{1}&c!";
+		Messages.CitiesList.text = "&aCities list:";
+		Messages.CitiesListFormat.text = "{0} [&e{1}&f]";
+		Messages.CitiesNotAvailableOnAdminClaims.text = "&cCities are not available on admin claims.";
+		Messages.CitizenExpelled.text = "&aCitizen expelled.";
+		Messages.CityAutojoinNotPermitted.text = "&c{0} does not permit auto-join, ask for an invite.";
+		Messages.CityAutojoinOff.text = "&aNow players need an invitation to join this city.";
+		Messages.CityAutojoinOn.text = "&aNow players can join this city without an invitation.";
+		Messages.CityHasBeenDisbanded.text = "&c{0} has been disbanded!";
+		Messages.CityInvitationAcceptOrReject.text = "&aType /c accept to accept; otherwise /c reject.";
+		Messages.CityInvitationAccepted.text = "&aYou accepted the invitation.";
+		Messages.CityInvitationExpiresOn.text = "&aThis player already recieved an invitation, it expires in: {0} seconds.";
+		Messages.CityInvitationHasBeenAccepted.text = "&aYour invitation to &b{0} &ahas been &baccepted&a.";
+		Messages.CityInvitationHasBeenRejected.text = "&aYour invitation to &b{0} &ahas been &crejected&a.";
+		Messages.CityInvitationNoPending.text = "&cYou do not have a pending invitation.";
+		Messages.CityInvitationReceived.text = "&aYou received a city invitation from &b{0} &ato join &b{1}&a.";
+		Messages.CityInvitationRejected.text = "&aYou rejected the invitation.";
+		Messages.CityInvitationSent.text = "&aCity invitation sent to &b{0}.";
+		Messages.CityNotExists.text = "&cThe city &b{0} &cdoes not exist.";
+		Messages.CitySpawnInvalid.text = "&cCity spawn must be located inside your city.";
+		Messages.CitySpawnMissing2.text = "&c{0}'s spawn is missing.";
+		Messages.CitySpawnSet.text = "&aCity spawn set.";
+		Messages.ClaimAlreadyACity.text = "&cThis claim is already a city.";
+		Messages.InviteNotAllowed.text = "&cYou're not allowed to invite people.";
+		Messages.MotdHasBeenSet.text = "&aA new motd has been set:";
+		Messages.MotdOut.text = "&aOuters motd: &b{0}";
+		Messages.MotdRes.text = "&aResidents motd: &b{0}";
+		Messages.MotdSet.text = "&aMotd set.";
+		Messages.NewCity.text = "&a{0} has just founded a new city called &b{1}&a.";
+		Messages.NoCities.text = "&aThere's no city! Be the first! Make a claim, and type /city new CityName.";
+		Messages.NoPermission.text = "&cYou don't have permission to use this command.";
+		Messages.NotAuthorized.text = "&cYou're not authorized.";
+		Messages.PlayerAlreadyOnAnotherCity.text = "&cThis player is already a member of another city.";
+		Messages.PlayerIsNotACitizen.text = "&cThis player is not a citizen.";
+		Messages.PlayerJoinedYourCity.text = "&b{0} &ajoined your city!";
+		Messages.PlayerLeftCity.text = "&b{0} &aleft the city.";
+		Messages.PlayerOfflineOrWrongName.text = "&cThis player is offline or the player name is wrong.";
+		Messages.PlotAssigned.text = "&aPlot assigned.";
+		Messages.PlotTakeNotAllowed.text = "&cYou can take this plot.";
+		Messages.PlotTakeYouHaveAPlotAlready.text = "&cYou can't take another plot if you have an assigned plot already.";
+		Messages.PlotCreated.text = "&aNew plot created.";
+		Messages.PlotDeleted.text = "&aPlot deleted.";
+		Messages.PlotMotdEdited.text = "&aPlot motd edited.";
+		Messages.PlotUnassigned.text = "&aPlot unassigned.";
+		Messages.StandOnPlotOrSubClaim.text = "&cYou have to stand on a subdivision. Use /subdivideclaims.";
+		Messages.ThisCityNotExists.text = "&cThis city doesn't exist.";
+		Messages.WelcomeBackTo.text = "&2Welcome back to &a{0}.";
+		Messages.WelcomeTo.text = "&2Welcome to &a{0}.";
+		Messages.WrongCitizenName.text = "&cWrong citizen name or this citizen is not part of your city.";
+		Messages.WrongCityName.text = "&cWrong city name.";
+		Messages.WrongCityNameCS.text = "&cWrong city name (it's case-sensitive).";
+		Messages.WrongCommand.text = "&cWrong command.";
+		Messages.YouAlreadyOnAnotherCity.text = "&cYou're a member of another city.";
+		Messages.YouCantRunCommandOnACity.text = "&cYou can't run this command on a city.";
+		Messages.YouLeftCity.text = "&aYou left &b{0}.";
+		Messages.YouNotACitizenHere.text = "&cYou're not a citizen here.";
+		Messages.YouNotCitizen.text = "&cYou're not a citizen.";
+		Messages.YouNotInACity.text = "&cYou're not in a city.";
+		Messages.YouNotOnAClaim.text = "&cYou're not on a claim.";
+		Messages.YouOnClaim.text = "&aYou're on &b{0}&a's claim.";
+		Messages.YouOnPlot.text = "&aYou entered &b{0}&a's plot.";
+		Messages.YouOnWilderness.text = "&aYou entered the wilderness.";
+		Messages.YouveBeenExpelled.text = "&eYou've been expelled from &b{0}.";
 		
-		messages.put(Messages.CitizensList, "&aCitizen's last played list for {0}.");
-		messages.put(Messages.CitizensListFormat, "&a{0} - {1} day(s) ago");
-		messages.put(Messages.CitizenInfo, "&b=== {0} ===\n&aResident on {1} from {2}\n&aLast played on {3}.");
-		messages.put(Messages.ClaimTooSmall, "&cThis claim is too small. Minimum size required is {0} blocks.");
-		messages.put(Messages.YouGotAPlot, "&a{0} assigned you a plot at {1}.");
-		messages.put(Messages.YouLostAPlot, "&a{0} unassigned your plot at {1}.");
-		messages.put(Messages.CitizenPermissions, "&a{0}'s permissions: {1}.");
-		messages.put(Messages.CityInfo, "&b===== {0} ===== ID: {1}\n"+
+		Messages.CitizensList.text = "&aCitizen's last played list for {0}.";
+		Messages.CitizensListFormat.text = "&a{0} - {1} day(s) ago";
+		Messages.CitizenInfo.text = "&b=== {0} ===\n&aResident on {1} from {2}\n&aLast played on {3}.";
+		Messages.ClaimTooSmall.text = "&cThis claim is too small. Minimum size required is {0} blocks.";
+		Messages.YouGotAPlot.text = "&a{0} assigned you a plot at {1}.";
+		Messages.YouLostAPlot.text = "&a{0} unassigned your plot at {1}.";
+		Messages.CitizenPermissions.text = "&a{0}'s permissions: {1}.";
+		Messages.CityInfo.text = "&b===== {0} ===== ID: {1}\n"+
 										"&aArea size: {2} m² | Plots: {3}\n"+
 										"&aCreated on: {4} | Mayor: {5}\n"+
 										"&aCitizens [{6}]:\n"+
 										"&a- Online: {7}\n"+
-										"&a- Offline: {8}");
-		messages.put(Messages.YouJoinedCity, "&aYou joined {0}.");
-		messages.put(Messages.YouAreBanned, "&cYou are banned from {0}!");
-		messages.put(Messages.PlayerIsBanned, "&cThis player was banned from {0}!");
-		messages.put(Messages.NewMayor, "&a{0} is {1}'s new mayor!");
-		messages.put(Messages.CityChatFormat, "&e[&f{0}&e] &f<{1}> {2}");
-		messages.put(Messages.CityNameInvalid, "&cCity name invalid.");
-		messages.put(Messages.ClaimAlreadyACity, "&cThis claim is already a city.");
-		messages.put(Messages.CitySameNameExists, "&cA city with the same name already exists.");
-		messages.put(Messages.CityRenamed, "&aCity renamed.");
-		messages.put(Messages.CityChatOn, "&aFrom now your messages are sent to the city chat.");
-		messages.put(Messages.CityChatOff, "&aFrom now your messages are sent to the public chat.");
-		messages.put(Messages.TakeableOn, "&aNew citizen can take this plot on their own.");
-		messages.put(Messages.TakeableOff, "&aNew citizen can't take this plot on their own.");
-		messages.put(Messages.PlayerBannedConfirm, "&a{0} was banned from {1} and can't autojoin or be invited.");
-		messages.put(Messages.PlayerUnbannedConfirm, "&a{0} was unbanned from {1}.");
-		messages.put(Messages.PlotInfo, "&bPlot ID: {0}\n"+
+										"&a- Offline: {8}";
+		Messages.YouJoinedCity.text = "&aYou joined {0}.";
+		Messages.YouAreBanned.text = "&cYou are banned from {0}!";
+		Messages.PlayerIsBanned.text = "&cThis player was banned from {0}!";
+		Messages.NewMayor.text = "&a{0} is {1}'s new mayor!";
+		Messages.CityChatFormat.text = "&e[&f{0}&e] &f<{1}> {2}";
+		Messages.CityNameInvalid.text = "&cCity name invalid.";
+		Messages.ClaimAlreadyACity.text = "&cThis claim is already a city.";
+		Messages.CitySameNameExists.text = "&cA city with the same name already exists.";
+		Messages.CityRenamed.text = "&aCity renamed.";
+		Messages.CityChatOn.text = "&aFrom now your messages are sent to the city chat.";
+		Messages.CityChatOff.text = "&aFrom now your messages are sent to the public chat.";
+		Messages.TakeableOn.text = "&aNew citizen can take this plot on their own.";
+		Messages.TakeableOff.text = "&aNew citizen can't take this plot on their own.";
+		Messages.PlayerBannedConfirm.text = "&a{0} was banned from {1} and can't autojoin or be invited.";
+		Messages.PlayerUnbannedConfirm.text = "&a{0} was unbanned from {1}.";
+		Messages.PlotInfo.text = "&bPlot ID: {0}\n"+
 				"&aAssigned on: {1} | Assigned to: {2}\n"+
-				"&aTakeable: {3}");
-		messages.put(Messages.MayorCannotLeave, "&aYou can't leave your city. Do /c mayor [CitizenName] to change the mayor.");
-		messages.put(Messages.YouOnUnassignedPlot, "&bYou're on an unassigned plot.");
-		messages.put(Messages.YouOnTakeablePlot, "&bYou can take this plot with /c plot take.");
-		messages.put(Messages.YouOnJoinableCity, "&bYou can join this city with /c join {0}.");
-		messages.put(Messages.CitySpawnTeleportDelay, "&bYou'll be teleported to {0}'s spawn in 5 seconds.");
-		messages.put(Messages.CitySpawnTeleportCancelled, "&bTeleport cancelled.");
-		messages.put(Messages.CitySpawnTeleported, "&bTeleported to {0}'s spawn.");
+				"&aTakeable: {3}";
+		Messages.MayorCannotLeave.text = "&aYou can't leave your city. Do /c mayor [CitizenName] to change the mayor.";
+		Messages.YouOnUnassignedPlot.text = "&bYou're on an unassigned plot.";
+		Messages.YouOnTakeablePlot.text = "&bYou can take this plot with /c plot take.";
+		Messages.YouOnJoinableCity.text = "&bYou can join this city with /c join {0}.";
+		Messages.CitySpawnTeleportDelay.text = "&bYou'll be teleported to {0}'s spawn in 5 seconds.";
+		Messages.CitySpawnTeleportCancelled.text = "&bTeleport cancelled.";
+		Messages.CitySpawnTeleported.text = "&bTeleported to {0}'s spawn.";
 	}
 	
 	static void load() {
-		if (messages.isEmpty()) {
-			defaults();
-		}
+		defaults();
 		
 		FileConfiguration messagesFile = YamlConfiguration.loadConfiguration(new File(Messages.messagesFilePath));
 		
 		for (Messages message : Messages.values()) {
-			String sendMessage = messages.get(message);
+			String sendMessage = message.text;
 			
 			if (sendMessage==null) {
-				GPPCities.getInstance().log(Level.WARNING, "Missing message ID "+message.toString());
+				GPPCities.getInstance().log(Level.WARNING, "Missing text ID "+message.toString());
 			}
 			
 			String mess=messagesFile.getString(message.toString());
 			if (mess==null) {
 				messagesFile.set(message.toString(), sendMessage);
-				messages.put(message, ChatColor.translateAlternateColorCodes('&', sendMessage));
+				message.text = ChatColor.translateAlternateColorCodes('&', sendMessage);
 			} else {
-				messages.put(message, ChatColor.translateAlternateColorCodes('&', mess));
+				message.text = ChatColor.translateAlternateColorCodes('&', mess);
 			}
 		}
 		try {
@@ -181,10 +178,10 @@ public enum Messages {
 	}
 	
 	public String get(String... strs) {
-		String message = messages.get(this);
+		String message = this.text;
 		if (message==null) {
 			GPPCities.getInstance().log(Level.WARNING, "Missing message ID "+this.toString());
-			return "Missing message! Please contact a server admin.";
+			return "Missing message ["+this.toString()+"]! Please contact a server admin.";
 		}
 		
 		for (int i=0; i<strs.length; i++) {
